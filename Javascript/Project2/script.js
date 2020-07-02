b2e36ba0bb8daa3f6ceea200eb7f0abea045e74b
patman007 <patrickmason007@gmail.com>
2020-07-01 20:24:36 -0500

    intial push

--- Javascript/Project2/script.js
+++ Javascript/Project2/script.js
@@ -5,11 +5,10 @@ function Myname(name) {
     //Local Scope variable
     var name = prompt("Tell me your name: ");
     if(name !== null){
-        document.write("You are going to have a wonderful day, " + name);
-        console.log("You are going to have a wonderful day, " + name);
-        alert("You are going to have a wonderful day, " + name);        
-    }
-    //return("You are going to have a wonderful day," + name);      
+        document.write("You are going to have a wonderful day, " + name + ".");
+        console.log("You are going to have a wonderful day, " + name + ".");
+        alert("You are going to have a wonderful day, " + name + ".");  
+    }          
 }
 Myname("");
 //Enter any name to invoke the function
@@ -22,36 +21,21 @@ Myname("");
 
 function MyNumber(num1, num2) {
     //Local Scope variable 1
-    var num1 = parseInt(prompt("Give me a number"));
+    var num1 = parseInt(prompt("Give me a number: "));
     //document.write("First Number: " + num1);
-    console.log("First Number: " + num1);
-    //alert("First Number: " + num1);
+    console.log("First Number: " + num1);    
 
     //Local Scrop variable 2
-    var num2 = parseInt(prompt("Give me another number"));
+    var num2 = parseInt(prompt("Give me another number: "));
     //document.write("Second Number is: " + num2);
-    console.log("Second Number: " + num2);
-    //alert("Second Number: " + num2);
+    console.log("Second Number: " + num2);    
 
     if (num1 !== null || num2 !== null) {                 
         var theTotal = parseInt(num1, 10) + parseInt(num2, 10);
-        document.write("  By the way, the sum of your numbers is " + num1 + " added to " + num2 + " equals "  + theTotal );       
-        console.log("  By the way, the sum of your numbers is " + num1 + " added to " + num2 + " equals "  + theTotal );
-        //alert("  By the way, the sum of your numbers is " + num1 + " added to " + num2 + " equals "  + theTotal );        
-    }
-    //Confused on this Return Statement if it can be used or not. 
-    //return("You are going to have a wonderful day," + name);
-    //Issues using the Nan function in an if else statement Advice from line 41 doen to 50
-    if (num1 == NaN || num2 == NaN) { 
-        document.write("Choose another number");
-        console.log("Choose another number");
-        
-        //IMPORTANT help What should the else statement be or should it just end where it is at?
-        //var theTotal = parseInt(num1, 10) + parseInt(num2, 10);
-        //document.write(" By the way, the sum of your numbers is "+ num1 + " added to " + num2 + " equals "  + theTotal );
-        //document.write(num1 + " added to " + num2 + " equals "  + theTotal);
-        //console.log("Passed Else")
-    } 
+        document.write(" By the way, the sum of your numbers is " + num1 + " added to " + num2 + " equals "  + theTotal );       
+        console.log(" By the way, the sum of your numbers is " + num1 + " added to " + num2 + " equals "  + theTotal );
+        alert(" By the way, the sum of your numbers is " + num1 + " added to " + num2 + " equals "  + theTotal );                
+    }    
 }
 MyNumber("", "");
 
---
+++ Javascript/Project3/script.js
@@ -0,0 +1,42 @@
+
+//First Number
+var num1 = parseInt(prompt("Give me a number: "));
+document.write("<div>First Number: " + num1 + "</div>");
+console.log("First Number: " + num1);
+
+//Second Number
+var num2 = parseInt(prompt("Give me another number: "));
+document.write("<div>Second Number is: " + num2 + "</div>");
+console.log("Second Number: " + num2);
+
+//Third Number
+var num3 = parseInt(prompt("Give me a number again: "));
+document.write("<div>Third Number is: " + num3 + "</div>");
+console.log("Third Number: " + num3);
+
+//For Loop Function
+var sum = [parseInt(num1, 10) + parseInt(num2, 10) +parseInt(num3, 10)];
+for (var i = 0; i < sum.length; i++) {
+	document.write("<div> The sum of all your numbers is " + sum[i] + "</div>");
+	console.log("The sum of all your numbers is " + sum[i]);
+}
+
+//Anonymous For each Loop Function
+//var sum = [parseInt(num1, 10) + parseInt(num2, 10) +parseInt(num3, 10)];
+//sum.forEach(function(total) {
+	//document.write("<div> The sum of all your numbers is " + total + "</div>")
+	//});
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
