2d9f0299312d2d869d10d92567fd5a99b24a78d0
patman007 <patrickmason007@gmail.com>
2020-07-16 16:12:09 -0500

    intial push

--- Javascript/Project7/script.js
+++ Javascript/Project7/script.js
@@ -1,105 +1,30 @@
 //Form Event Javascript  Way 1
-//Email Variables
+//Email Variable and Password Variable
 var theEmail = document.getElementById("email");
-var warning = document.getElementById("theForm");
-
-// Does not Appear when use Bootstrap
-theEmail.addEventListener("focus", function() {
-    warning.textContent = "Please enter a valid email."
-});
-
-
-//Blur Event Email
-theEmail.addEventListener("blur", function() {
-    if (theEmail.value == "") {
-        alert("Please Check the fields and make sure they are not blank");
-    } else {
-        warning.textContent = "";        
-    }
-});
-
-
-//Password Variables
 var thePassword = document.getElementById("password");
-var warning1 = document.getElementById("theForm");
 
-//Focus Event Password
-thePassword.addEventListener("focus", function() {
-    warning1.textContent = "Please enter a valid password."
-});
+//Use Query Selector to create a function on the submit button
+document.querySelector("form").addEventListener("submit", function(event) {
 
+    var errors =[];
+    //conditional to check name input
+    if(theEmail.value === null || theEmail.value === "") {
+        errors.push("Please enter an email address");
+    };
 
-//Appears when I use Bootstrap
-thePassword.addEventListener("blur", function() {
-    if (thePassword.value == "") {
-        alert("Please Check the fields and make sure they are not blank");
-    } else {
-        warning.textContent = "";        
+    //conditional to check password
+    //It cannot be null, empty or less than 6 characters
+    if(thePassword.value === null || thePassword.value === "" || 
+    thePassword.value.length <= 6) {
+        errors.push("Password must be entered and greater than 6 characters");
     }
+
+    if(errors.length > 0) {
+        //Display the error messages
+        event.preventDefault();        
+        document.querySelector("p").textContent = errors.join(", ");
+        document.querySelector("p").style.color = "red";
+    }
+
 });
-
-
-/////////////////////////////////////////////////////////////////////////////////
-
-// // Form Event Javascript 2 for Bootsratp Method rough draft
-// // Use of calling a function instead of doing 
-// // Email Variables
-// var warning = document.getElementById("theForm");
-// var theEmail = document.getElementById("email");
-
-
-// theEmail.addEventListener("focus", displayEmail);
-// //Email Function 1
-// function displayEmail() {
-//     warning.textContent = "Please enter a valid email." 
-//     //I do not see this message when the input is selected    
-// };
-
-
-// theEmail.addEventListener("blur", displayEmail1);
-// //Email Function 2
-// function displayEmail1() {
-//     if (theEmail.value == "") {
-//         alert("Please Check the fields and make sure they are not blank");
-//     } else {
-//         warning.textContent = "";
-//         console.log("Clear"); // I do not see this message in the console            
-//     }
-// };
-
-
-
-// ///Password Variables
-// var warning1 = document.getElementById("theForm");
-// var thePassword = document.getElementById("password");
-
-
-// thePassword.addEventListener("focus", displayPassword);
-// //Password Function 1
-// function displayPassword() {
-//     warning1.textContent = "Please enter a valid password."
-//     //I do not see this message when the input is selected 
-// };
-
-
-// thePassword.addEventListener("blur", displayPassword1);
-// //Password Function 2
-// function displayPassword1() {
-//     if (thePassword.value == "") {
-//         alert("Please Check the fields and make sure they are not blank");
-//     } else {
-//         warning.textContent = "";
-//         console.log("Clear"); // I do not see this message in the console            
-//     }
-// };
-
-
-
-
-
-
-
-
-
-
 
