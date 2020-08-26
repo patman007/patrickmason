e250fe36e7279fc65be13283f325b02278bb9c10
patman007 <patrickmason007@gmail.com>
2020-08-26 15:55:07 -0500

    Project 4

--- Node_JS_APIs/Project4/Food_Finder/app.js
+++ Node_JS_APIs/Project4/Food_Finder/app.js
@@ -1,14 +1,12 @@
 //Foundation
 const express = require('express')
 const yelp = require('yelp-fusion')
-
 //API key code
 const apiKey = 'DPt7lsXbCbBYJG_V0GlmR9q0bz_8Tbc3H66yZ-yTXYTyrN2xfNzdgAl_FuXLCWJYd4oVqAAvPrS4q9u8tXH4cfFvOyVzuRROWQR_eQP9Td5Tf9DkQr67TKenVelCX3Yx' 
-
-//Able to use API Key in the client
+//Able to use API Key in the client constant
 const client = yelp.client(apiKey) 
-console.log(client.transactionSearch)
-// iQbursVWrtaKbPA009p7GQ //Client ID if needed
+console.log(client)
+// iQbursVWrtaKbPA009p7GQ //Client ID if needed?
 
 const app = express();
 
@@ -16,57 +14,54 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 //Route Handlers
-
 //yelp api consume autocomplete api
-let endpoint = 'https://api.yelp.com/v3/transactions/{transaction_type}/search' 
+let endpoint = 'https://api.yelp.com/v3/businesses/search'
 console.log(endpoint)
+
 //Home.ejs
 app.get('/', (req, res) => {
-    console.log('I am home ejs')
-    res.render('home.ejs')
+    console.log('I am home ejs');
+    res.render('home.ejs');        
 })
-
-//API documentation NPM use on website
-// client.transactionSearch('delivery', {
-//     location: 'san diego'
-//   }).then(response => {
-//     console.log(response.jsonBody.businesses[0].name);
-//   }).catch(e => {
-//     console.log(e);
-//   });
+ 
+//NPM business search code in documentation
+// client.search({
+//   term: 'Four Barrel Coffee',
+//   location: 'san francisco, ca',
+// }).then(response => {
+//   console.log(response.jsonBody.businesses[0].name);
+// }).catch(e => {
+//   console.log(e);
+// });
 
 //Searchresults.ejs 
-app.get('/searchresults/lattitude/longtitude/location', (req, res) => { 
+app.get('searchresults', (req, res) => {  
     //API documentation NPM use on website
-    client.transactionSearch( 'delivery', {
-        location: 'austin'        
-    }) 
-    console.log(client.transactionSearch)  
-    console.log(`${endpoint}/${req.query.latitude}/${req.query.longitude}/${req.query.location}`)   
-    let url = `${endpoint}/${req.query.latitude}/${req.query.longitude}/${req.query.location}`       
-    fetch(url)
-    .then(response => {
-        if(!response.ok) {
-            //Test error first
-            //throw errors will find other errors
-            //different from catch errors
-            throw Error('Issue with receiving Response')
-        }
-        //json does parsing and return a string
-        console.log(response.jsonBody.description)
-        return response.jsonBody.businesses[0].name()
-        
+    client.search({     
+      location: location
     })
-    .then(data => {
-        console.log('data is', {data})
-        res.render('searchresults.ejs', {data})
-    })
-    //Catch error will only show connection error
-    .catch(err => console.log('I am the error: ', err))
-    console.log(err)
-    
-})
+    //location variable has an error at this point
+    // but also doe not work outside of the function
+    //code keeps breaking down on Line 46    
+    var location = req.query.location
 
+    //API and location for url not sure if
+    //important to use here or not
+    let url = `${endpoint}/${location}`
+    console.log(url) 
+    //Response
+    .then(response => {
+      console.log(response.jsonBody.businesses[0].name)   
+      res.render('/searchresults.ejs', response)
+    })
+    //Catch error had said  code 400, which is say my
+    //The request could not be understood by the server 
+    //due to malformed syntax. 
+    //The client SHOULD NOT repeat the request without modifications.
+    .catch(err => {
+      console.log('I am the error: ', err);
+    });    
+})
 
 //Listeners
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
