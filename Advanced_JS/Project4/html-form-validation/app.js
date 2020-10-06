c751a2049b708656b249ff7c1be69589f49027f9
patman007 <patrickmason007@gmail.com>
2020-10-06 15:21:06 -0500

    Advanced Javascript 5

--- Advanced_JS/Project4/html-form-validation/app.js
+++ Advanced_JS/Project4/html-form-validation/app.js
@@ -2,12 +2,17 @@
 const express = require('express');
 const app = express()
 
+const bodyParser = require('body-parser')  
+
 //public folder to use CSS styling
 app.use (express.static('public'));
 
+//Body Parser app.use
+app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.urlencoded({ extended: false }))
+
 //Setup up PORT 3000
 const PORT  = process.env.PORT || 3000
-
 
 //Route Handlers
 //Home.ejs route handler
@@ -19,49 +24,33 @@ app.get('/', (req, res) => {
 })
 
 //Submit app.get route handler
-app.get("/submit", (req, res) => {
-    console.log('I am the submit page')
+app.get("/submit", (req, res) => {    
     // res.render('submit.ejs');
-    res.render('submit');
+    res.render('submit', {data: req.body.applicant})
+    console.log('I am the submit page GET', {data : req.body.applicant})
 });
 
 
-//Submit.ejs route handler
-app.post('/submit', (req, res) => {
-    let applicant = 
-        {
-            fname: req.body.fname,
-            lname: req.body.lname,
-            email: req.body.email,
-            tel: req.body.tel,
-            country: req.body.country,
-            resume: req.body.resume
-        }        
-    res.redirect("/submit", {data: applicant})
-    console.log('I am the submit page', {data: applicant})
+//Submit app.post route handler
+app.post('/submit', (req, res) => {  
+    // let applicant = 
+    // 
+    //     "fname": fname,
+    //     "lname": lname,
+    //     "email": email,
+    //     "tel": tel,
+    //     "country": country,
+    //     "resume": resume
+    // 
+    res.redirect('/submit')
+    console.log('I am the submit page POST', {data : req.body.applicant})
 })
 
-//It just tells express that you're using EJS, 
-//and it removes the need to write ".ejs" 
-//on the file names
+// It just tells express that you're using EJS, 
+// and it removes the need to write ".ejs" 
+// on the file names
 app.set('view engine', 'ejs');
 
 //Listener
 app.listen(PORT, () => console.log(`App listening on ${PORT}`));
 
-
-// Other way to do possibly
-// const express = require('express');
-// const app = express();
-// const bp=require('body-parser')
-// const PORT = process.env.PORT || 3000;
-// app.use(express.static("public"));
-// app.use(bp.json({ type: 'application/*+json' }))
-// app.use(bp.urlencoded({ extended: false }))
-// app.get('/', (req, res)=>{
-//     res.render("home.ejs")
-// })
-// app.post('/submit', (req, res)=>{
-//     res.render("submit.ejs", {data:req.body} )
-// })
-// app.listen(PORT, ()=> console.log(`App listening on port ${PORT}`))
