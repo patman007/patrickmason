2d3c17aabaa21928351a1276da03ac8f9b9b7b0f
patman007 <patrickmason007@gmail.com>
2020-11-05 13:24:27 -0600

    Backend Practice

--- BackendPractice/SuperHero_Backend/app.js
+++ BackendPractice/SuperHero_Backend/app.js
@@ -6,40 +6,45 @@ const request = require('request')
 
 app.use(express.static('public'));
 
-
 //Setup up Port 3000
 const PORT = process.env.PORT || 3000
 
-
 //Root Routes
+
+//Home.ejs Root Handler
 app.get('/', (req, res) => {
     console.log('I am the home ejs')
     res.render('home.ejs');
 })
 
 
-app.get('results', (req, res) => {
-    // console.log('I am the results.ejs')
-    console.log(url)
+//Results.ejs Root Handler
+app.get('/results', (req, res) => {
+    console.log('I am the results ejs')    
     
     // This is the access token below
     // const access_token = 10158830495094328
     let endpoint = 'https://superheroapi.com/api/10158830495094328/'
     
-    let url = `${endpoint}/${req.query.id}`    
+    // let url = `${endpoint}/${req.query.id}` 
+    let url = `${endpoint}1/powerstats`  
+    console.log(url) 
 
+    //Request the API instead of Fetch
     request(url, function(error, response, body){
         if(!error && res.statusCode === 200) {
             // console.log(response)
 
             let parsedData = JSON.parse(body);
-            // console.log(parsedData)
+            // console.log(body)
 
             res.render('results.ejs', {data: parsedData});
-            // console.log('The data is', {data:parsedData});
+            console.log('The data is', {data: parsedData});
+
         } else {
-            //error handling
-            res.render('results.ejs', {data: 'Error getting data'})
+
+            //error handling            
+            res.render('results.ejs', {data: 'Error getting data'});
         }
     });
 
