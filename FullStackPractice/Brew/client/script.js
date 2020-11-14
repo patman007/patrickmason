7069133c92ef27254e574cad6e91cd37ad6e7031
patman007 <patrickmason007@gmail.com>
2020-11-13 22:17:39 -0600

    Full Stack Practice

---
+++ FullStackPractice/Brew/client/script.js
@@ -0,0 +1,24 @@
+//Variable to define for ids
+//Name
+let name = document.getElementById('name');
+//City
+let city = document.getElementById('CITY');
+//State
+let state = document.getElementById('STATE');
+//Type
+let type = document.getElementById('TYPE');
+//Phone
+let phone = document.getElementById('phone');
+//Street
+let street = document.getElementById('street');
+//Latitude
+let lat = document.getElementById('lat');
+//Longitude
+let long = document.getElementById('long');
+//Url
+let url = document.getElementById('url');
+//Button
+let btn = document.getElementById('btn')
+
+
+
