f472843f2a360022c75223dc0d69f7fb0c900ef9
patman007 <patrickmason007@gmail.com>
2020-11-30 17:23:37 -0600

    Final Exam Practical Update

--- 03_final/P1_PRACTICAL/PunkAPI/app.js
+++ 03_final/P1_PRACTICAL/PunkAPI/app.js
@@ -3,9 +3,6 @@ const express = require('express');
 const app = express()
 const fetch =  require('node-fetch')
 
-//Axios
-const axios =  require('axios');
-
 //Morgan
 const logger = require("morgan");
 app.use(logger("dev"));
@@ -26,18 +23,13 @@ app.use(cors())
 //Examples are CSS and HTML files
 app.use(express.static('public'))
 
-
-//Set up a port if it has or has not
 //specified on PORT 3000 or another
 //will be used for the environment (env)
 const PORT = process.env.PORT || 3000;
 
 //Consume an endpoint for the API
-let endpoint = 'https://api.punkapi.com/v2/beers/'
+let endpoint = 'https://api.punkapi.com/v2/beers'
 console.log(endpoint)
-
-
-//Root Route Handlers
 
 //Root Route Handler ('/')
 //app.get will handle only GET HTTP requests 
@@ -51,18 +43,11 @@ app.get('/', (req, res) => {
 //Results Handler
 //app.get will handle only GET HTTP requests
 app.get('/results', (req, res) => {
-    //Variable used to setup the query
-    //in url to be fetched
-    let beer_name = req.query.name
-    let url = `${endpoint}?name=${beer_name}`
-
-    //FETCH FRAMEWORK (4 Parts)
-    //fetch(url)
-    //.then((response) => {})
-    //.then(() => {})
-    //.catch(() => {})
-
-    console.log(url)
+    //Variable used to setup the query in url to be fetched
+    console.log('req.query is ', req.query)
+    let beer_name = req.query.beer_name
+    let url = `${endpoint}?beer_name=${beer_name}`
+    console.log('my uri is ', url)
     fetch(url)
     .then (response => {
         
@@ -84,10 +69,8 @@ app.get('/results', (req, res) => {
     //Looking for results with success with the string valuess
     //send inside of the url endpoint.
     .then(data => {
-        
+        data.map(d => console.log(d.id, d.name))
         //do something with the data and renders the results EJS page
-        //success
-        console.log('data is', {data : data})
         res.render('results.ejs', {data : data})
     })
 
@@ -97,54 +80,54 @@ app.get('/results', (req, res) => {
         
         //Console.log message shows a status message 
         //to be handled
-        res.status(404).send({error})   
+        res.status(404).send({error})  
+        res.render('/error', {error}) 
         console.log('Catch error: ', error)             
     })
 })
 
-//POST METHOD
-axios({
-    method: 'post',
-    url: '/results',
-    data: {
-        name: 'Beer',
-        description: 'Description'
-    }
-  })
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+app.post('/savestars', (req, res) => {  
+    let beer_body = req.body.name
+    let beer_description = req.body.description
+    let image = req.body.image
 
-// const BeerUrl = 'http://localhost:3000/results'
+    let beer_name = req.query.beer_name
 
-//POST METHOD OTHER WAY
-// app.post('/results', (req, res) => {
-//     console.log("I am the saves ejs")
-//     let name = req.body.name       
-//     let newBeer = {name: name} 
-//     $.ajax({
-//         url: BeerUrl,
-//         method: "POST",
-//         data: newBeer
-//     })
-//     .done(function(newBeer){
-//         let checked = newBeer.isComplete ? "checked" : "";
-//         $('input').append(
-//         `<input data-id=${newBeer.name}class=${checked}>${newBeer.name} name="checked" 
-//         type="checkbox" value="true" onChange="this.form.submit()"></input>`
-//         );
-//         $('input').val('');
-    
-//     })    
-//     visited.push(newBeer)   
-//     // console.log(req.body.name); //Output=> like { searchid: 'Array of checked checkbox' }
-//     // console.log(req.body.image); // to get array of checked checkbox    
-//     res.redirect('/results')
-// });
+    fetch("https://api.punkapi.com/v2/beers/?beer_name=", {
+        method: 'post',
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        },
+        body: JSON.parse({
+            "name": `${beer_body}`,
+            "description": `${beer_description}`,
+            "image": `${image}`
+        })
+      })
+      .then(response => {
+        
+        //IF Statment: Tests for errors first before returning
+        //response.json to see if we need to work on our code.
+        if(!response.ok) {
+            // console.log(response)
 
+            //We are testing errors first            
+            //different from catch errors.
+            //Use a res.send instead of a throw Error
+            res.send({code: 55, 
+                message: 'Your response failed, please contact support at support@'})
+        }
+        //json does parsing and returns a string
+        return response.json()
+    })
+      .then(function (data) {
+        console.log('Request succeeded with JSON response', data);
+        res.render('/savestars', data)
+      })
+      .catch(function (error) {
+        console.log('Request failed', error);
+      });
+})
 
 //Listener
 //app.listen is a function used to bind and listen to the 
